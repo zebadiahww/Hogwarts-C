413d8d6a153a092c4f41782852697c9118a1b120
Karl Pfister <pfister@Karls-MacBook-Pro.local>
2019-10-07 21:52:26 -0600

    Initial Commit

---
+++ Hogwarts-C/Hogwarts-C/Supporting Files/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hogwarts-C
+//
+//  Created by Karl Pfister on 10/7/19.
+//  Copyright © 2019 Karl Pfister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Hogwarts-C/Hogwarts-C/Supporting Files/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Hogwarts-C
+//
+//  Created by Karl Pfister on 10/7/19.
+//  Copyright © 2019 Karl Pfister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
