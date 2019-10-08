5b3000ef971c90cd8d8691638eb86cd39332eef4
Zebadiah <zebadiahww@gmail.com>
2019-10-08 12:04:14 -0600

    model complete

--- Hogwarts-C/Hogwarts-C/Model/ZWWCharacter.m
+++ Hogwarts-C/Hogwarts-C/Model/ZWWCharacter.m
@@ -8,6 +8,43 @@
 
 #import "ZWWCharacter.h"
 
+static NSString * const kName = @"name";
+static NSString * const kHouse = @"house";
+static NSString * const kBloodStatus = @"bloodStatus";
+static NSString * const kDeathEater = @"deathEater";
+static NSString * const kRole = @"role";
+
 @implementation ZWWCharacter
+
+- (ZWWCharacter *)initWithName:(NSString *)name house:(NSString *)house bloodStatus:(NSString *)bloodstatus deathEater:(BOOL)deathEater role:(NSString *)role
+{
+    if (self = [super init])
+    {
+        //initialize self
+        _name = name;
+        _house = house;
+        _bloodStatus = bloodstatus;
+        _deathEater = deathEater;
+        _role = role;
+    }
+    return self;
+}
+
+@end
+
+@implementation ZWWCharacter  (JSONConvertible)
+
+-(instancetype) initWithDictionary:(NSDictionary<NSString *,id> *)dictionary
+{
+    // let name = dicionary["name"]
+    
+    NSString *name = dictionary[kName];
+    NSString *house = dictionary[kHouse];
+    NSString *bloodStatus = dictionary[kBloodStatus];
+    BOOL deathEater = dictionary[kDeathEater];
+    NSString *role = dictionary[kRole];
+    
+    return [self initWithName:name house:house bloodStatus:bloodStatus deathEater:deathEater role:role];
+}
 
 @end
